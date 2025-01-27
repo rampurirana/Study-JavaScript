9c335bd860410cf2b3694dcf7440c00e66986745
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-01-27 19:01:21 +0530

    variables in JavaScript

--- Day 01/index.js
+++ Day 01/index.js
@@ -1,3 +1,18 @@
 console.log("Welcome to JavaScript code")
-console.log("Rampravesh Rana")
 console.log("I love JavaScript")
+
+// Variables in JavaScript
+
+fullName = "Rampravesh Rana"; // string
+age = 20; // number
+address = "Jharkhand";
+x = null; // null
+y = undefined; // undefined
+isFollow = false; // boolean 
+
+console.log(fullName);
+console.log(age);
+console.log(address);
+console.log(x);
+console.log(y);
+console.log(isFollow);
