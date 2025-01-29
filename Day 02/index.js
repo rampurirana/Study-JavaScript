2a00e55dfa3e5e79904c89dd3e1a07b588eabbec
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-01-29 18:38:31 +0530

    Primitive data types

---
+++ Day 02/index.js
@@ -0,0 +1,33 @@
+// 1. Primitive data types
+
+// A. String : Represents a sequence of number
+
+let fullName = "Rampravesh Rana";
+console.log(fullName);
+
+// B. Number : Represents both integer and float number
+
+let age = 10;
+let price = 85.67;
+console.log(age, price);
+
+// C. Boolean : Represents either true of false
+
+let isStudent= true;
+console.log(isStudent);
+
+// D. Undefined : A variable that has been declared but not assigned a value
+let x;
+console.log(x);
+
+// E. Null : Represents the intentional absence of any object value
+let y = null;
+console.log(y);
+
+// F. BigInt : Used for large integers
+let bigNumber = BigInt(154695445451);
+console.log(bigNumber);
+
+// G. Symbol : Represents a unique identifier
+let sym = Symbol("Rana");
+console.log(sym);
