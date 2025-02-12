971afcd561f0bb9e438e273c7e76287aca0250a9
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-12 18:40:37 +0530

    Comparison Operator

--- Day 04/index.js
+++ Day 04/index.js
@@ -1,12 +1,14 @@
+// Comparison Operator : Use to compare two values
+
 let a = 10;
 let b = 20;
 
 console.log("a =", a,"b =", b);
-console.log("a == b :", a==b);
-console.log("a === b :", a===b);
-console.log("a != b :", a!=b);
-console.log("a !== b :", a!==b);
-console.log("a > b :", a>b);
-console.log("a < b :", a<b);
-console.log("a >= b :", a>=b);
-console.log("a <= b :", a<=b);
+console.log("a == b :", a==b); // Equal to ( == )
+console.log("a === b :", a===b); // Strict equal to ( === ) Check data types also
+console.log("a != b :", a!=b); // Not equal to ( != )
+console.log("a !== b :", a!==b); // Strict not equal to ( !== ) Check data type also
+console.log("a > b :", a>b); // left is Greater than right ( > )
+console.log("a < b :", a<b); // left is less than right ( < )
+console.log("a >= b :", a>=b); // left is greater than or equal to the right ( >= )
+console.log("a <= b :", a<=b); // left is less than or equal to the right ( <= )
