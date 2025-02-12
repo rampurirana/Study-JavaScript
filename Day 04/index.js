adea3759fbb2ca0a9c5679d124f00428bc2fb640
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-12 18:33:51 +0530

    Comparison Operator

---
+++ Day 04/index.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 20;
+
+console.log("a =", a,"b =", b);
+console.log("a == b :", a==b);
+console.log("a === b :", a===b);
+console.log("a != b :", a!=b);
+console.log("a !== b :", a!==b);
+console.log("a > b :", a>b);
+console.log("a < b :", a<b);
+console.log("a >= b :", a>=b);
+console.log("a <= b :", a<=b);
