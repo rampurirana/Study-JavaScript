5e5f2f9d1782e1575b0dde8adc11e02cb904ef1d
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-12 19:30:25 +0530

    if...else statement

--- Day 05/if_State.js
+++ Day 05/if_State.js
@@ -5,3 +5,11 @@ let age = 18;
 if (age >= 18){
     console.log("You are an adult");
 }
+
+// 2. if...else statement : Executus one block if the conditions is true, othrwise executes the else block
+let votAge = 12;
+if (votAge >= 18){
+    console.log("You are eligible for vote");
+} else {
+    console.log("You are not eligible for vote");
+}
