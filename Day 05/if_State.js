e7b186b7764bc5da4692c57ce665429b3308d6a8
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-12 19:25:20 +0530

    Conditional Statement

---
+++ Day 05/if_State.js
@@ -0,0 +1,7 @@
+// Conditional Statement : Conditional statement allow to execute different code based on conditions
+
+// 1. if statement : Executes a block of code if the conditions is true
+let age = 18;
+if (age >= 18){
+    console.log("You are an adult");
+}
